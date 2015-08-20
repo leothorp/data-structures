6fb13f05f003196092d0adfa6adc901a47634333
leothorp <leo.thorp1@gmail.com>
2015-08-20 12:40:05 -0500

    functional complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,27 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var end = 0;
+  var start = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function(){
+    if (end - start) {
+      var result = storage[start];
+      delete storage[start];
+      start++;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return end - start;
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  
   // Implement the methods below
   someInstance.push = function(value){
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function(){
+    var result = storage[count];
+    delete storage[count];
+    if (count) {
+      count--;
+    }  
+    return result;
   };
 
   someInstance.size = function(){
+    return count;
   };
-
+  
   return someInstance;
 };
