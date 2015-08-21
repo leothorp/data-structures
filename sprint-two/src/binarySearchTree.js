bd1265ad8c94b368a83ca7667e80884c209fe6dd
leothorp <leo.thorp1@gmail.com>
2015-08-21 14:02:13 -0500

    Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,35 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
+  var obj = {};
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  obj.insert = function(input) {
+    if (input === obj.value) return 'ERROR';
+    var side = input < obj.value ? 'left' : 'right';
+    if (obj[side]) {
+      obj[side].insert(input);
+    } else {
+      obj[side] = BinarySearchTree(input);
+    }
+  };
+
+  obj.contains = function(input) {     
+    if (obj.value === input) return true; 
+    var side = input < obj.value ? 'left' : 'right';
+    if (obj[side]) {
+      return obj[side].contains(input);
+    } else {
+      return false;
+    }
+  };
+
+  obj.depthFirstLog = function(func) {
+    func(obj.value);
+    if (obj.left) obj.left.depthFirstLog(func);
+    if (obj.right) obj.right.depthFirstLog(func);
+  };
+
+  return obj;
 };
 
 
